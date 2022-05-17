1716112c091aa2f8371f92fd3f9c6b20c987c40f
r.jozwiak <r.jozwiak@aotm.gov.pl>
2022-05-17 22:31:28 +0200

    update view

--- backend/aiohttp_demo/views.py
+++ backend/aiohttp_demo/views.py
@@ -1,24 +1,18 @@
 from aiohttp import web
 import json
 
+from db_connection import db_service
+
 
 async def get_user(request):
-    try:
+    async with request.app['db'].acquire() as conn:
         user_id = request.match_info['user_id']
-        response_obj = {
-            'status': 'success', 'message': f'User ID to: {user_id}'
-        }
-
-        return web.Response(
-            text=json.dumps(response_obj),
-            status=200
-        )
-
-    except Exception as e:
-        response_obj = {
-            'status': 'failed', 'message': str(e)
-        }
-        return web.Response(text=json.dumps(response_obj), status=500)
+        try:
+            user = await db_service.get_user(conn, user_id)
+        except db_service.RecordNotFound as e:
+            raise web.HTTPNotFound(text=str(e))
+        results = {key: value for key, value in user.items()}
+        return web.Response(body=json.dumps(results), status=200)
 
 
 # async def create_user(request):
